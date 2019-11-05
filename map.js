28cdb99165b1828d37b623c14f7f94292cf19a23
Alaedine <alaedinekarouia@gmail.com>
2019-11-05 13:46:14 +0100

    Update map.js
    mise a jour de la map fonctionnel

--- map.js
+++ map.js
@@ -1,13 +1,21 @@
 
+let ctx = this.ctx;
+
 
 //map temporaire
-const map = 
-[[ 2,2,0,0,2,2],
- [2,0,2,1,0,2],
- [0,1,2,0,0,1],
- [0,0,1,1,0,0],
- [2,0,0,1,0,2],
- [2,2,1,1,2,2]];           // chaque numéro correspond au type de la case (mur,mur casable,chemin etc...)
+var map = 
+[[2,2,0,0,2,1,2,2,0,0,2,1],
+ [2,0,2,1,0,2,2,2,0,3,2,0],
+ [0,1,2,0,0,1,2,1,0,0,2,1],
+ [0,0,1,1,0,0,2,1,2,0,2,2],
+ [2,0,0,1,0,2,2,2,0,0,2,1],
+ [2,2,1,1,2,2,0,2,0,1,2,3],
+ [2,0,0,1,0,2,2,1,0,0,2,1],
+ [2,0,0,1,0,2,2,2,1,0,2,0],
+ [2,0,2,1,0,2,1,2,0,0,2,0],
+ [2,0,0,1,0,2,2,3,0,0,2,2],
+ [2,1,0,1,0,2,1,2,0,1,2,1],
+ [2,0,0,1,0,2,2,1,0,0,2,1]];    // chaque numéro correspond au type de la case (mur,mur casable,chemin etc...)
 
 
 var taille = map.length;       //nombre  de lignes
@@ -23,38 +31,40 @@ class Case{
 }
     
 
-    draw(){                              //dessin de la map en fonction de la matrice
-        let x = -longueur;              //On initialise x a - longueur pour arriver à 0 sur le dessin de la première case
+    draw(ctx){                   //dessin de la map en fonction de la matrice
+        let x = 0;              //On initialise x a - longueur pour arriver à 0 sur le dessin de la première case
         let y = 0;
+        
 
-        for (let i = 0; map.length;i++ ){
+        for (let i = 0; i < map[0].length;i++ ){
             y = 0;                        //Quand on fini une colone on repart de 0
-            x += longueur;               //A chaque fois qu'on fini une colonne on se décale de la longueur de la case
-            for ( let j = 0 ; map[i].length ; j++){
+            x = this.longueur * i;               //A chaque fois qu'on fini une colonne on se décale de la longueur de la case
+           
+            for ( let j = 0 ; j < map[0].length ; j++){
+                y = j * this.longueur;
                 if (map[i][j] == 0) {
                     ctx.fillStyle = "grey";    //mur incassable
-                    ctx.FillRect(x,y,longueur,longueur);
-                    y += longueur;       //On incrémente y pour faire la case suivante de la colonne
+                    ctx.fillRect(x,y,this.longueur,this.longueur);
+                         //On incrémente y pour faire la case suivante de la colonne
                 }
 
                 if (map[i][j] == 1) {
-                    ctx.fillStyle = "brown";  //mur cassable
-                    ctx.FillRect(x,y,longueur,longueur);
-                    y += longueur;
+                    ctx.fillStyle = "yellow";  //mur cassable
+                    ctx.fillRect(x,y,this.longueur,this.longueur);
                 }
                 if (map[i][j] == 2) {
-                    ctx.fillStyle = "green";  //chemin normale
-                    ctx.FillRect(x,y,longueur,longueur);
-                    y += longueur;
+                    ctx.fillStyle = 'blue';  //chemin normale
+                    ctx.fillRect(x,y,this.longueur,this.longueur);
                 }
 
-                else  {
+                  if (map[i][j] == 3){
                     ctx.fillStyle = "red"   // chemin enflammé
-                    ctx.FillRect(x,y,longueur,longueur);
-                    y += longueur;
+                    ctx.fillRect(x,y,this.longueur,this.longueur);
+                    
                 }
 
             }
         }
     }
 }
+
